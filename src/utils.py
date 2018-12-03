93f24d56858a2ce5eed4969fb116b32e841e3ad0
Reuben G <rtg2@uw.edu>
2018-12-02 18:05:04 -0800

    Merging utils with lab2 utils

--- src/utils.py
+++ src/utils.py
@@ -123,3 +123,70 @@ def map_to_world(pose,map_info):
     config[2] += angle
 
     return config
+
+
+'''
+  Converts a particle to a pose message
+    particle: The particle to convert - [x,y,theta]
+    Returns: An equivalent geometry_msgs/Pose
+'''
+def particle_to_pose(particle):
+    pose = Pose()
+    pose.position.x = particle[0]
+    pose.position.y = particle[1]
+    pose.orientation = angle_to_quaternion(particle[2])
+    return pose
+
+'''
+  Converts a list of particles to a list of pose messages
+    particles: A list of particles, where each element is itself a list of the form [x,y,theta]
+    Returns: A list of equivalent geometry_msgs/Pose messages
+'''
+def particles_to_poses(particles):
+    return map(particle_to_pose, particles)
+
+'''
+  Creates a header with the given frame_id and stamp. Default value of stamp is
+  None, which results in a stamp denoting the time at which this function was called
+    frame_id: The desired coordinate frame
+    stamp: The desired stamp
+    Returns: The resulting header
+'''
+def make_header(frame_id, stamp=None):
+    if stamp == None:
+        stamp = rospy.Time.now()
+    header = Header()
+    header.stamp = stamp
+    header.frame_id = frame_id
+    return header
+
+'''
+  Converts a list with coordinates into a point message
+  npt: A list of length two containing x and y coordinates
+  Returns: A geometry_msgs/Point32 message
+'''
+def point(npt):
+    pt = Point32()
+    pt.x = npt[0]
+    pt.y = npt[1]
+    return pt
+
+'''
+  Converts a list of coordinates into a list of equivalent point messages
+  arr: A list of coordinates, where each element is itself a two dimensional list
+  Returns: A list of geometry_msgs/Point32 messages
+'''
+def points(arr):
+    return map(point, arr)
+
+''' Get the map from the map server
+In:
+  map_topic: The service topic that will provide the map
+Out:
+  map_img: A numpy array with dimensions (map_info.height, map_info.width). 
+           A zero at a particular location indicates that the location is impermissible
+           A one at a particular location indicates that the location is permissible
+  map_info: Info about the map, see
+            http://docs.ros.org/kinetic/api/nav_msgs/html/msg/MapMetaData.html 
+            for more info    
+'''
